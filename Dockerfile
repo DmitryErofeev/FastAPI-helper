06245025fb9fcff3090f6acd93eba0568fb5f818
Dima <erofeev@smartintel.ru>
2021-08-02 09:09:42 +0300

    модуль переименовывания коммутатора закончен

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+from python:3.9-alpine
+
+WORKDIR /app
+
+RUN python -m venv venv
+COPY requirements.txt requirements.txt
+RUN venv/bin/pip install -r requirements.txt
+
+COPY . .
+
+EXPOSE 8000
+ENTRYPOINT ["venv/bin/python", "-m", "uvicorn", "--host","0.0.0.0","main:app"]
--- main.py
+++ main.py
@@ -1,38 +1,72 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import pynetbox
 import datetime
 import json
-from celery import Celery
+# from celery import Celery
 
 
 with open('config.json', 'r') as cf_file:
     config = json.load(cf_file)
 
-nb_url = config['netbox']['url']
-nb_token =config['netbox']['token']
+
+NB_URL = config['netbox']['url']
+NB_TOKEN =config['netbox']['token']
+NB_USER = config['netbox']['nb_user']
 
 app = FastAPI()
-nb = pynetbox.api(nb_url, nb_token)
-
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
+nb = pynetbox.api(NB_URL, NB_TOKEN)
+now = datetime.datetime.now()
 
 
-# @app.post("/items/{item_id}")
-# def read_item(item_id: int, q: Optional[str] = None, a: Optional[str] = None):
-#     """
-
-#     """
-#     return {"item_id": item_id, "q": q, "a": a}
-
-
-
-@app.post("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None, a: Optional[str] = None):
+@app.post("/device_update")
+async def device_update(request: Request):
     """
 Ловит вебхук из нетбокса и переименовывает коммутатор.
     """
+    response_code = "500"
+    data = await request.json()
 
-    return {"item_id": item_id, "q": q, "a": a}
+    current_date = now.strftime('%Y%m%d')
+    date_remove = now.strftime('%Y-%m-%d')
+
+    device_status = data['data']['status']['value']
+
+    if data['username'] != NB_USER:
+
+        if device_status == 'offline':
+
+            device = nb.dcim.devices.get(data['data']['id'])
+
+            new_device_name = device['name'] + '-' + current_date
+            new_status = 'offline'
+            interfaces = nb.dcim.interfaces.filter(device_id=device.id, cabled=True)
+            ids_cable_links = [id.cable_peer.cable for id in interfaces]
+
+
+            try:
+                for cable_link in ids_cable_links:
+                    link = nb.dcim.cables.get(cable_link)
+                    link.delete()
+                print('Линки коммутатора удалены')
+
+            except Exception:
+                print('Линки коммутатора не удалены')
+
+            try:
+                device = nb.dcim.devices.get(device.id)
+
+                device.name = new_device_name
+                device.status = new_status
+                device.custom_fields['dateRemove'] = date_remove
+                device.save()
+                print('Коммутатор переименован')
+
+            except Exception as ex:
+                print(ex)
+                print('Коммутатор не переименован')
+                return {'result': 'failure'}
+
+            return {"result":'ok'}
+
+    return {'result': 'ignore'}
